71da5b3f23c122e0109f149af93f77af1f449735
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-03-08 16:46:57 +0800

    Always get a new connection from the driver rather than reuse

--- src/main/java/elections/data/ConnectionUtil.java
+++ src/main/java/elections/data/ConnectionUtil.java
@@ -2,8 +2,6 @@ package elections.data;
 import java.sql.*;
 
 public class ConnectionUtil {
-    private static Connection _connection = null;
-
     private static final String CONN_USERNAME = "root";
     private static final String CONN_PASSWORD = "";
     private static final String DB_SERVER = "localhost";
@@ -11,19 +9,15 @@ public class ConnectionUtil {
     private static final String DB_NAME = "elections";
 
     public static Connection getConnection() throws SQLException {
-        if (_connection == null) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch (Exception ex) {
-                return null;
-            }
-            
-            _connection = DriverManager.getConnection(
-                    "jdbc:mysql://" + DB_SERVER + ":" + DB_PORT + "/" + DB_NAME,
-                    CONN_USERNAME,
-                    CONN_PASSWORD);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (Exception ex) {
+            return null;
         }
-
-        return _connection;
+        
+        return DriverManager.getConnection(
+                "jdbc:mysql://" + DB_SERVER + ":" + DB_PORT + "/" + DB_NAME,
+                CONN_USERNAME,
+                CONN_PASSWORD);
     }
 }

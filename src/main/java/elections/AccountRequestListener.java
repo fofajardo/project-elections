afaadf59fd079f353145f9b0ff55a44ca5e160fc
Francis Fajardo <francisdominic.fajardo@gmail.com>
2022-03-08 16:47:32 +0800

    Remove extraneous space

--- src/main/java/elections/AccountRequestListener.java
+++ src/main/java/elections/AccountRequestListener.java
@@ -26,6 +26,6 @@ public class AccountRequestListener implements ServletRequestListener {
         }
     }
 
-    public void requestDestroyed(ServletRequestEvent event)  {
+    public void requestDestroyed(ServletRequestEvent event) {
     }
 }
